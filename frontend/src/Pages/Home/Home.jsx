6e75bfdd46b428671919a1927cd35513c4ee7931
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-11-07 21:11:43 +0530

    tailwind configuration is added

---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600 text-pretty">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
