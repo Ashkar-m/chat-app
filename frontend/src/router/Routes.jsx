454b27fb3ed1a70f94e71d1cff6a615e1f31e523
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-11-18 17:56:19 +0530

    Routes changed

--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Register/Register';
+import Chat from "../Pages/Chat/Chat";
 
 const MyRoutes = () => {
   return (
@@ -8,6 +11,9 @@ const MyRoutes = () => {
 
         <Routes>
             <Route element={<Home />} path="/home" />
+            <Route element={<Login />} path="/login" />
+            <Route element={<Register />} path="/register" />
+            <Route element={<Chat />} path="/chat" />
         </Routes>
       
     </Router>
